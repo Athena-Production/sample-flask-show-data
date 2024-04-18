a681e3eb8829f92616c4a3c911d8046dead13d99
Blake Fullerton <43489000+blaketylerfullerton@users.noreply.github.com>
2024-04-17 17:31:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Function to connect to the database
+def connect_db():
+     db = mysql.connector.connect(
+        username="doadmin",
+        password="AVNS_G_K59rLh3HLpfthyzqU",
+        host="athena-do-user-16198044-0.c.db.ondigitalocean.com",
+        port=25060,
+        database="defaultdb",
+    )
+    cursor = db.cursor()
 
+# Route to display data from the database
+@app.route('/')
+def index():
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM scheduled_calls')
+    data = cursor.fetchall()
+    conn.close()
+    return render_template('index.html', data=data)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
